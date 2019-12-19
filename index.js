f1c2a6d6632de49c75167246cf4c64976524b508
Nikhil Satya <nikhilssn10@gmail.com>
2019-12-19 12:50:25 +0530

    path for index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT||3000
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
